08cd5b765efa977c36ce834e582894fd1644445e
VALDEIR <valdeir_014@hotmail.com>
2023-10-15 15:00:44 -0300

    Associando referencia e consulta personalizada

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -2,8 +2,17 @@ import livros from "../models/Livro.js";
 
 class LivroController{
 
-    static listarLivros = (req, res) =>{
+    /*static listarLivros = (req, res) =>{
         livros.find((err, livros) =>{
+            res.status(200).json(livros)
+        })
+    }*/
+
+    //Popular com dados do autor
+    static listarLivros = (req, res) =>{
+        livros.find()
+            .populate('autor')
+            .exec((err, livros) =>{
             res.status(200).json(livros)
         })
     }
@@ -11,7 +20,18 @@ class LivroController{
     static listarLivroPorId = (req, res) =>{
         const id = req.params.id;
         
-        livros.findById(id,(err, livros) =>{
+        /*livros.findById(id,(err, livros) =>{
+            if(err){
+                res.status(400).send({message:`${err.message} - id do livro não localizado`})
+            }else{
+                res.status(200).send(livros);
+            }
+        })*/
+
+        //trazer somente o nome do autor na coleção
+        livros.findById(id)
+            .populate('autor','nome')
+            .exec((err, livros) =>{
             if(err){
                 res.status(400).send({message:`${err.message} - id do livro não localizado`})
             }else{
@@ -55,6 +75,14 @@ class LivroController{
             }
         })
 
+    }
+
+    static listarLivrosPorEditora = (req, res) =>{
+        const editora = req.query.editora
+
+        livros.find({'editora': editora}, {}, (err, livros) => {
+            res.status(200).send(livros);
+        })
     }
 
 }
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -3,8 +3,11 @@ import LivroController from "../controllers/livrosController.js";
 
 const router = express.Router();
 
+//Rotas da mais especifica para menos especifica, ex: se colocar a chamada de busca de pois de id vai chamar a de id primeiro 
+
 router
     .get("/livros", LivroController.listarLivros)
+    .get("/livros/busca", LivroController.listarLivrosPorEditora)
     .get("/livros/:id", LivroController.listarLivroPorId)
     .post("/livros", LivroController.cadastrarLivro)
     .put("/livros/:id", LivroController.atualizarLivro)
